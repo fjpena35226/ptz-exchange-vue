dfdd7178f3a79984409503728ea4f3edea73b219
Francisco Javier Peña Vietía <fjpena35226@gmail.com>
2020-06-05 17:06:03 -0300

    Init git repo

---
+++ src/components/PxButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <beat-loader :loading="isLoading" :color="`#68d391`" :size="8" />
+    <button
+      v-if="!isLoading"
+      @click="buttonClick"
+      class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
+    >
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PxButton",
+
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    buttonClick() {
+      this.$emit("click");
+    }
+  }
+};
+</script>
